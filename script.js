6fcea4accd86a4b5dc9c13245468f406264314e0
Mateus Saraiva <mateus.saraiva09@gmail.com>
2023-05-04 17:47:26 -0300

    main idea completed

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const listMovieElement = document.querySelector('.listMovies')
+const input = document.querySelector("#input")
 const button = document.querySelector('#button')
 const baseURL = "https://image.tmdb.org/t/p/w200/"
-let number = "0"
 
 const key = "b7c257791c7c9a9552fbce87526bb019"
-const search = "batman"
 
 const test = async () => {
     const movie = await fetch('https://api.themoviedb.org/3/movie/502356?api_key=b7c257791c7c9a9552fbce87526bb019&language=pt-BR').then(response => response.json())
@@ -20,34 +19,53 @@ const test = async () => {
 }
 
 const searchMovie = async () => {
-    const movie = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${key}&language=pt-BR&query=${search}`).then(response => response.json())
+
+    const oldElement = document.querySelectorAll(".movie")
+
+    for (let i = oldElement.length - 1; i >= 0; i--) {
+        oldElement[i].remove()
+    }
+
+    const search = input.value;
+    const movie = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${key}&page=1&language=pt-BR&query=${search}`).then(response => response.json())
+
     const amountPagesSearch = movie.total_pages
-    const movies = movie.results
-    const amountMoviesInPage = movies.length
 
-    for (let i = 0; i < amountMoviesInPage; i++) {
+    for (let page = 1; page < amountPagesSearch; page++) {
+        const allMoviesInPages = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${key}&page=${page}&language=pt-BR&query=${search}`).then(response => response.json())
 
-        const titleMovie = movies[i].title
-        const backgroundMovie = movies[i].poster_path
+        const moviesAllPages = allMoviesInPages.results
+        const amountMoviesInPage = moviesAllPages.length
 
-        const newMovie = document.createElement('div')
-        newMovie.classList.add(`movie`)
-        newMovie.classList.add(`movie${i}`)
-        listMovieElement.appendChild(newMovie)
+        for (let i = 0; i < amountMoviesInPage; i++) {
 
-        const movieElement = document.querySelector(`.movie${i}`)
+            const titleMovie = moviesAllPages[i].title
+            const backgroundMovie = moviesAllPages[i].poster_path
 
-        const newBackGroundMovie = document.createElement('img')
-        newBackGroundMovie.classList.add(`poster${i}`)
-        newBackGroundMovie.setAttribute("src", `https://image.tmdb.org/t/p/w500${backgroundMovie}`);
-        newBackGroundMovie.setAttribute("alt", "poster");
-        movieElement.appendChild(newBackGroundMovie)
+            const newMovie = document.createElement('div')
+            newMovie.classList.add(`movie`)
+            newMovie.classList.add(`movie${i}_page${page}`)
+            listMovieElement.appendChild(newMovie)
 
-        const titleNewMovie = document.createElement('h1')
-        titleNewMovie.innerHTML = titleMovie
-        titleNewMovie.classList.add(`title${i}`)
-        movieElement.appendChild(titleNewMovie)
+            const movieElement = document.querySelector(`.movie${i}_page${page}`)
+
+            const newBackGroundMovie = document.createElement('img')
+            newBackGroundMovie.classList.add(`poster${i}_page${page}`)
+            newBackGroundMovie.setAttribute("src", `https://image.tmdb.org/t/p/w500${backgroundMovie}`);
+            newBackGroundMovie.setAttribute("alt", `poster do filme ${titleMovie}`);
+            movieElement.appendChild(newBackGroundMovie)
+
+            const titleNewMovie = document.createElement('h1')
+            titleNewMovie.innerHTML = titleMovie
+            titleNewMovie.classList.add(`title${i}`)
+            movieElement.appendChild(titleNewMovie)
+        }
     }
 }
 
 button.addEventListener('click', searchMovie)
+document.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        searchMovie()
+    }
+})
